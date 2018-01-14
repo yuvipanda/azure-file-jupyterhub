c1f1cf856fa1c71c4af4fc1671f3588fbc575055
yuvipanda <yuvipanda@gmail.com>
2018-01-14 12:44:49 -0800

    Switch back to using bind mounts to expose homedirs
    CIFS barfs if the user subdir isn't already present. This
    sorts around that problem.
    
    Also switch to scipy-notebook as base, and add admins list.

--- user/mount.bash
+++ user/mount.bash
@@ -5,8 +5,11 @@
 #
 # Takes the following parameters as environment variables
 #
-# CIFS_URL
-# URL to mount
+# CIFS_PARENT_URL
+# URL to mount to make sure we have a homedir present
+#
+# CIFS_SUBDIR
+# Subdir of URL to create
 #
 # MOUNT_POINT
 # Path to mount whole share at
@@ -29,17 +32,26 @@
 # and exec the commands it is passed to at the end.
 set -e
 
-#CIFS_PASSWORD=$(cat /etc/cifs/secrets/azurestorageaccountkey)
-#CIFS_USERNAME=$(cat /etc/cifs/secrets/azurestorageaccountname)
+CIFS_URL="${CIFS_PARENT_URL}/${CIFS_SUBDIR}"
 
 echo "Creating ${MOUNT_POINT}"
 mkdir -p ${MOUNT_POINT}
 
-echo "Trying to mount..."
+echo "Trying to mount parent dir"
+
+mkdir -p /mnt/parent
+chown root:root /mnt/parent
+chmod 0700 /mnt
+
 mount -t cifs \
-      ${CIFS_URL} \
-      ${MOUNT_POINT} \
+      ${CIFS_PARENT_URL} \
+      /mnt/parent \
       -o vers=3.0,cache=strict,uid=${UID},noforceuid,gid=${GID},noforcegid,file_mode=0700,dir_mode=0700,persistenthandles,nounix,serverino,mapposix,rsize=1048576,wsize=1048576,echo_interval=60,actimeo=1,username=${CIFS_USERNAME},password=${CIFS_PASSWORD}
+echo "Parent dir mounted!"
+
+echo "Trying to mount..."
+mkdir -p /mnt/parent/${CIFS_SUBDIR}
+mount --bind /mnt/parent/${CIFS_SUBDIR} ${MOUNT_POINT}
 echo "Mounted!"
 
 echo "Starting required process"
